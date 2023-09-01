c5b6235a69b4c33739b4219e5fe7e19be7683294
Subhasish Dutta <subhasishduttashuvo2018@gmail.com>
2023-09-01 11:05:50 +0600

    Create add the two number using the function.py

---
+++ add the two number using the function.py
@@ -0,0 +1,9 @@
+#declare the function 
+def add (x,y):
+ return x+y
+#intiaze the varibable of the twi varriabble
+number1 =10;
+number2 =20;
+sum_of_two_number=add(number1,number2)
+#print the result
+print("sum of the {0} and {1} is {2} " .format(number1,number2,sum_of_two_number))
